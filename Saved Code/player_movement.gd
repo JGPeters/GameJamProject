88a6ef372a130eb1d28ee31fced6ba755a7780fb
JGPeters <95588296+JGPeters@users.noreply.github.com>
2024-09-18 22:22:00 -0500

    Gobbo has personality

---
+++ Saved Code/player_movement.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_dir = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		input_dir.x += 1
+	if Input.is_action_pressed("left"):
+		input_dir.x -= 1
+	if Input.is_action_pressed("down"):
+		input_dir.y += 1
+	if Input.is_action_pressed("up"):
+		input_dir.y -= 1
+		
+	if input_dir.length() > 0:
+		input_dir = input_dir.normalized() * speed
+		
+	move_and_collide(input_dir * delta)
+	
---
+++ player_movement.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+@export var speed = 400
+@onready var player : Node2D = get_owner()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+		
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		
+	player.position += velocity * delta
+	
